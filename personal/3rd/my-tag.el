0cd1f558e09f19c8ae9dbb6813c8dbb56deb4501
feng@feng-mbp <feng@feng-mac>
2016-01-27 15:49:55 -0800

    A hack

--- personal/3rd/my-tag.el
+++ personal/3rd/my-tag.el
@@ -10,13 +10,23 @@
 (setq tags-add-tables nil)
 
 (require 'etags-table)
+
 (setq tag-table-alist
-      (list
-       '((concat (getenv "HOME") "/.emacs.d") (concat (getenv "HOME") "/.emacs.d/TAGS"))
-       '((concat (getenv "HOME") "/code/fg_demo/") (concat (getenv "HOME") "/code/pyia/TAGS"))
-       '(".*\\.py$" (concat (getenv "HOME") "/code/py_lib/TAGS") (concat (getenv "HOME") "/code/py_caf/TAGS") (concat (getenv "HOME") "/code/caffe/TAGS"))
-       '(".*\\.m$" (concat (getenv "HOME") "/code/mat_lib/TAGS") (concat (getenv "HOME") "/code/mat/TAGS"))
-       '(".*\\.lua$" (concat (getenv "HOME") "/code/lua_lib/TAGS") (concat (getenv "HOME") "/code/lua_th/TAGS"))))
+      (cond
+       ((string-equal system-type "darwin")
+        (list
+         '("/Users/feng/.emacs.d/" "/Users/feng/.emacs.d/TAGS")
+         '("/Users/feng/code/fzhou/fg_demo/" "/Users/feng/code/pyia/TAGS")
+         '(".*\\.py$" "/Users/feng/code/py_lib/TAGS" "/Users/feng/code/py_caf/TAGS" "/Users/feng/code/caffe/TAGS")
+         '(".*\\.m$" "/Users/feng/code/mat_lib/TAGS" "/Users/feng/code/mat/TAGS")
+         '(".*\\.lua$" "/Users/feng/code/lua_lib/TAGS" "/Users/feng/code/lua_th/TAGS")))
+       ((string-equal system-type "gnu/linux")
+        (list
+         '("/home/ma/feng/.emacs.d/" "/home/ma/feng/.emacs.d/TAGS")
+         '("/home/ma/feng/code/fzhou/fg_demo/" "/home/ma/feng/code/pyia/TAGS")
+         '(".*\\.py$" "/home/ma/feng/code/py_lib/TAGS" "/home/ma/feng/code/py_caf/TAGS" "/home/ma/feng/code/caffe/TAGS")
+         '(".*\\.m$" "/home/ma/feng/code/mat_lib/TAGS" "/home/ma/feng/code/mat/TAGS")
+         '(".*\\.lua$" "/home/ma/feng/code/lua_lib/TAGS" "/home/ma/feng/code/lua_th/TAGS")))))
 (setq etags-table-alist tag-table-alist)
 (setq etags-table-search-up-depth 10)
 
