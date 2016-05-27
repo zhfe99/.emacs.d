35cf7350617fe289bc336d41930fc2263e0ad29f
Feng Zhou <zhoufeng09@baidu.com>
2016-05-27 17:25:42 +0800

    Fix modelline error

--- personal/3rd/my-modeline.el
+++ personal/3rd/my-modeline.el
@@ -7,9 +7,9 @@
 
 (setq sml/theme 'respectful)
 (size-indication-mode -1)
-(setq sml/name-width 50)
+;; (setq sml/name-width 50)
 (setq sml/line-number-format " %3l")
-(setq sml/mode-width nil)
+;; (setq sml/mode-width nil)
 (setq sml/mule-info nil)
 (setq sml/prefix nil)
 (sml/setup)
