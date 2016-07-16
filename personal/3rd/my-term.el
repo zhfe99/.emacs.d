35faa86eaa821225e3ff64982d19fd4f0fe3821e
Feng Zhou <zhfe99@gmail.com>
2016-07-16 13:41:02 +0800

    Remove C-a binding

--- personal/3rd/my-term.el
+++ personal/3rd/my-term.el
@@ -15,6 +15,18 @@
   (interactive)
   (term-send-raw-string "\C-a\C-d"))
 
+;; https://github.com/xee5ch/prelude/commit/00dee5841fec8631b8ee66b752c5fb7320202686
+;; remove C-a binding
+(defun my-term-mode-hook ()
+  (let ((oldmap (cdr (assoc 'prelude-mode minor-mode-map-alist)))
+                (newmap (make-sparse-keymap)))
+        (set-keymap-parent newmap oldmap)
+        (define-key newmap (kbd "C-a") nil)
+        (make-local-variable 'minor-mode-overriding-map-alist)
+        (push `(prelude-mode . ,newmap) minor-mode-overriding-map-alist))
+  )
+(add-hook 'term-mode-hook 'my-term-mode-hook)
+
 ;; default shell form multi-term
 (cond
  ((string-equal system-type "darwin")
