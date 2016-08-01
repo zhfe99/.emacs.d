a903b321268f7672682bb41e054a0a78ec8abee5
Feng Zhou <zhfe99@gmail.com>
2016-08-01 14:41:21 -0700

    Use company-irony for c/c++ mode

--- personal/3rd/my-c.el
+++ personal/3rd/my-c.el
@@ -45,6 +45,23 @@
             (setq c-basic-offset 2)
             (git-gutter-mode)))
 
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
 ;; remap M-j
 (define-key c++-mode-map (kbd "M-j") 'avy-goto-word-1)
 (define-key c-mode-map (kbd "M-j") 'avy-goto-word-1)
