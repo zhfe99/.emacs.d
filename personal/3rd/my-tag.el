54a541500da645c6ae6654548d2dd03999656d4e
Feng Zhou <zhfe99@gmail.com>
2016-08-01 21:24:23 -0700

    Add submodule helm-etags+

--- personal/3rd/my-tag.el
+++ personal/3rd/my-tag.el
@@ -20,20 +20,25 @@
 (setq etags-table-search-up-depth 10)
 
 (require 'etags-select)
-(require 'helm-etags+)
-(add-hook 'helm-etags+-select-hook 'etags-table-recompute)
+
+(use-package helm-etags+
+  :load-path "site-lisp/helm-etags-plus/"
+  :config
+  (add-hook 'helm-etags+-select-hook 'etags-table-recompute))
 
 ;; auto-update tags after saving
-(require 'ctags-update)
-(autoload 'turn-on-ctags-auto-update-mode "ctags-update" "turn on 'ctags-auto-update-mode'." t)
-(add-hook 'sh-mode-hook  'turn-on-ctags-auto-update-mode)
-(add-hook 'emacs-lisp-mode-hook  'turn-on-ctags-auto-update-mode)
-(add-hook 'python-mode-hook  'turn-on-ctags-auto-update-mode)
-(add-hook 'c++-mode-hook  'turn-on-ctags-auto-update-mode)
-(add-hook 'lua-mode-hook  'turn-on-ctags-auto-update-mode)
-(add-hook 'matlab-mode-hook  'turn-on-ctags-auto-update-mode)
-(add-hook 'c-mode-common-hook  'turn-on-ctags-auto-update-mode)
-(setq ctags-update-delay-seconds (* 1 60))
+(use-package ctags-update
+  :load-path "site-lisp/helm-etags-plus/"
+  :config
+  (autoload 'turn-on-ctags-auto-update-mode "ctags-update" "turn on 'ctags-auto-update-mode'." t)
+  (add-hook 'sh-mode-hook 'turn-on-ctags-auto-update-mode)
+  (add-hook 'emacs-lisp-mode-hook 'turn-on-ctags-auto-update-mode)
+  (add-hook 'python-mode-hook 'turn-on-ctags-auto-update-mode)
+  (add-hook 'c++-mode-hook 'turn-on-ctags-auto-update-mode)
+  (add-hook 'lua-mode-hook 'turn-on-ctags-auto-update-mode)
+  (add-hook 'matlab-mode-hook 'turn-on-ctags-auto-update-mode)
+  (add-hook 'c-mode-common-hook 'turn-on-ctags-auto-update-mode)
+  (setq ctags-update-delay-seconds (* 1 60)))
 
 (provide 'my-tag)
 ;;; my-tag.el ends here
