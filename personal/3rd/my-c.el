c0d138a7466ec190bd33a1ad8e9e6381fc5ecda2
Feng Zhou <zhfe99@gmail.com>
2016-08-01 20:38:52 -0700

    Set M-a for magit-status

--- personal/3rd/my-c.el
+++ personal/3rd/my-c.el
@@ -66,6 +66,9 @@
 (define-key c++-mode-map (kbd "M-j") 'avy-goto-word-1)
 (define-key c-mode-map (kbd "M-j") 'avy-goto-word-1)
 (define-key protobuf-mode-map (kbd "M-j") 'avy-goto-word-1)
+(define-key c++-mode-map (kbd "M-a") 'magit-status)
+(define-key c-mode-map (kbd "M-a") 'magit-status)
+(define-key protobuf-mode-map (kbd "M-a") 'magit-status)
 
 (provide 'my-c)
 ;;; my-c.el ends here
