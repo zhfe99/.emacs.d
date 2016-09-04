557bd1ebcc2fab7ff9e8e7812cd4dfe775682202
Feng Zhou <zhfe99@gmail.com>
2016-09-04 13:33:58 +0800

    Stop using irony

--- personal/3rd/my-company.el
+++ personal/3rd/my-company.el
@@ -7,21 +7,5 @@
 
 (setq company-idle-delay 0.5)
 
-;; use irony
-(eval-after-load 'company
-  '(add-to-list 'company-backends 'company-irony))
-(eval-after-load 'company
-  '(add-to-list 'company-backends 'company-irony-c-headers))
-
-;; replace the `completion-at-point' and `complete-symbol' bindings in
-;; irony-mode's buffers by irony-mode's function
-(defun my-irony-mode-hook ()
-  (define-key irony-mode-map [remap completion-at-point]
-    'irony-completion-at-point-async)
-  (define-key irony-mode-map [remap complete-symbol]
-    'irony-completion-at-point-async))
-(add-hook 'irony-mode-hook 'my-irony-mode-hook)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
 (provide 'my-company)
 ;;; my-company.el ends here
