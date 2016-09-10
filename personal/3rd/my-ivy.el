d69370cbcc256c3e4250a7cb53d6493396a7e8b2
Feng Zhou <zhfe99@gmail.com>
2016-09-10 15:07:49 +0800

    Add avy-action-copy-line

--- personal/3rd/my-ivy.el
+++ personal/3rd/my-ivy.el
@@ -193,5 +193,32 @@ Or else, find files since 24 weeks (6 months) ago."
 ;; https://github.com/abo-abo/swiper/issues/256
 ;; (require 'ivy_buffer_extend)
 
+;; copy line in avy-action
+(defun avy-action-copy-line (pt)
+  "Copy current line on pt."
+  (save-excursion
+    (let (str pt2)
+      (goto-char pt)
+      (beginning-of-line)
+      (setq pt2 (point))
+      (end-of-line)
+      (setq str (buffer-substring pt2 (point)))
+      (kill-new str)
+      (message "Copied: %s" str)))
+  (let ((dat (ring-ref avy-ring 0)))
+    (select-frame-set-input-focus
+     (window-frame (cdr dat)))
+    (select-window (cdr dat))
+    (goto-char (car dat))))
+
+;; add avy-action-copy-line
+(setq avy-dispatch-alist
+  '((?x . avy-action-kill-move)
+    (?X . avy-action-kill-stay)
+    (?m . avy-action-mark)
+    (?n . avy-action-copy)
+    (?N . avy-action-copy-line)
+    (?i . avy-action-ispell)))
+
 (provide 'my-ivy)
 ;;; my-dired.el ends here
