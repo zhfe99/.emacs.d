188f2c582ae32a484f7486a4da417b5884e8c036
Feng Zhou <zhfe99@gmail.com>
2016-09-24 14:18:02 +0800

    Clean my-git

--- personal/3rd/my-git.el
+++ personal/3rd/my-git.el
@@ -13,34 +13,7 @@
     (require 'git-gutter-fringe+)
   (require 'git-gutter+))
 
-;; http://blog.binchen.org/posts/enhance-emacs-git-gutter-with-ivy-mode.html
-(defun my-reshape-git-gutter (gutter)
-  "Re-shape gutter for `ivy-read'."
-  (let* ((lineno (aref gutter 3))
-         line)
-    (save-excursion
-      (goto-line lineno)
-      (setq line (buffer-substring (line-beginning-position)
-                                   (line-end-position))))
-    ;; build (key . lineno)
-    (cons (format "%s %d: %s"
-                  (if (eq 'deleted (aref gutter 1)) "-" "+")
-                  lineno
-                  (replace-regexp-in-string "^[ \t]*" "" line))
-          lineno)))
-
-;; use ivy to show diffs
-(defun my-goto-git-gutter ()
-  (interactive)
-  (if git-gutter:diffinfos
-      (let* ((collection (mapcar 'my-reshape-git-gutter
-                                 git-gutter:diffinfos)))
-        (ivy-read "git-gutters:" collection
-                  :action (lambda (lineno)
-                            (goto-line (cdr lineno)))))
-    (message "NO git-gutters!")))
-
-;; http://blog.binchen.org/posts/enhance-emacs-git-gutter-with-ivy-mode.html
+;; modified from http://blog.binchen.org/posts/enhance-emacs-git-gutter-with-ivy-mode.html
 (defun my-reshape-git-gutter+ (gutter)
   "Re-shape gutter for `ivy-read'."
   (let* ((lineno (nth 5 gutter))
@@ -67,21 +40,8 @@
                             (goto-line (cdr lineno)))))
     (message "NO git-gutters!")))
 
-;; see what line changed in latest commit
-;; http://blog.binchen.org/posts/git-gutter-tip.html
-(defun git-gutter-reset-to-head-parent()
-  (interactive)
-  (let (parent (filename (buffer-file-name)))
-    (if (eq git-gutter:vcs-type 'svn)
-        (setq parent "PREV")
-      (setq parent (if filename (concat (shell-command-to-string (concat "git --no-pager log --oneline -n1 --pretty='format:%H' " filename)) "^") "HEAD^")))
-    (git-gutter:set-start-revision parent)
-    (message "git-gutter:set-start-revision HEAD^")))
-
-(defun git-gutter-reset-to-default ()
-  (interactive)
-  (git-gutter:set-start-revision nil)
-  (message "git-gutter reset"))
+;;==============
+;; Magit setting
 
 ;; use ivy in magit
 (setq magit-completing-read-function 'ivy-completing-read)
