0ff1823cf5b7b259e900eb19f28a4b6a8f8cdc5f
Feng Zhou <zhfe99@gmail.com>
2017-02-18 20:34:22 +0800

    Remove unused line

--- personal/3rd/my-term.el
+++ personal/3rd/my-term.el
@@ -23,8 +23,6 @@
  ((string-equal system-type "gnu/linux")
   (setq multi-term-buffer-name "term"
         multi-term-program (concat (getenv "APPS") "/bin/zsh"))))
-;; (add-hook 'term-mode-hook
-;;           (lambda () (setq truncate-lines 0)))
 
 ;; yank bash history
 (defun counsel-yank-bash-history ()
