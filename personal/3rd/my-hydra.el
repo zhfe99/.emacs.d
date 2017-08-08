3819133d1ce457a432c2ce15bff3439694c9e0d0
Feng Zhou <zhoufeng09@baidu.com>
2017-08-09 06:30:42 +0800

    Bind num-key in hydra-window

--- personal/3rd/my-hydra.el
+++ personal/3rd/my-hydra.el
@@ -46,6 +46,13 @@ _u_ dupe"
   ("t" eyebrowse-rename-window-config :exit t)
   ("," eyebrowse-prev-window-config :exit t)
   ("." eyebrowse-next-window-config :exit t)
+  ("1" eyebrowse-switch-to-window-config-1 :exit t)
+  ("2" eyebrowse-switch-to-window-config-2 :exit t)
+  ("3" eyebrowse-switch-to-window-config-3 :exit t)
+  ("4" eyebrowse-switch-to-window-config-4 :exit t)
+  ("5" eyebrowse-switch-to-window-config-5 :exit t)
+  ("6" eyebrowse-switch-to-window-config-6 :exit t)
+  ("7" eyebrowse-switch-to-window-config-7 :exit t)
   ("q" nil))
 
 ;; open
