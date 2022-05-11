b15085b0a1d3e171b558b551dbfb39e934aa21b5
Feng Zhou <zhfe99@gmail.com>
2022-05-11 19:24:45 +0800

    Add dired-toggle-sudo

--- personal/3rd/my-dired.el
+++ personal/3rd/my-dired.el
@@ -260,5 +260,15 @@
 ;; support imenu in dired
 (require 'dired-imenu)
 
+;; ==========
+;; sudo
+(require 'dired-toggle-sudo)
+(define-key dired-mode-map (kbd "C-c C-s") 'dired-toggle-sudo)
+(eval-after-load 'tramp
+  '(progn
+     ;; Allow to use: /sudo:user@host:/path/to/file
+     (add-to-list 'tramp-default-proxies-alist
+	          '(".*" "\\`.+\\'" "/ssh:%h:"))))
+
 (provide 'my-dired)
 ;;; my-dired.el ends here
