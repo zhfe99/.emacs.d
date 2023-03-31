bd2c3efd1804306b3ba6898daec6affdd96f0ecb
Feng Zhou <zhfe99@gmail.com>
2023-03-31 23:28:19 +0800

    Fix bug

--- personal/3rd/my-modeline.el
+++ personal/3rd/my-modeline.el
@@ -11,14 +11,14 @@
   :config
   (load-theme 'zenburn t))
 
-(setq sml/theme 'respectful)
-(size-indication-mode -1)
-(setq sml/name-width 20)
-(setq sml/line-number-format " %3l")
-(setq sml/mode-width 3)
-(setq sml/mule-info nil)
-(setq sml/prefix nil)
-(sml/setup)
+;; (setq sml/theme 'respectful)
+;; (size-indication-mode -1)
+;; (setq sml/name-width 20)
+;; (setq sml/line-number-format " %3l")
+;; (setq sml/mode-width 3)
+;; (setq sml/mule-info nil)
+;; (setq sml/prefix nil)
+;; (sml/setup)
 
 (provide 'my-modeline)
 ;;; my-modeline.el ends here
